b12d3e67a0004184efd8734a8c3b7a66c924ea7e
Luis Salas <luispiyu007@gmail.com>
2023-01-24 17:37:26 -0600

    JDBC|Transacciones commit y rollback | 189

--- JDBC2/src/main/java/datos/PersonaDAO.java
+++ JDBC2/src/main/java/datos/PersonaDAO.java
@@ -8,11 +8,23 @@ import java.util.List;
 
 //DAO Data access Object
 public class PersonaDAO {
+
+    private Connection conexionTransaccional;
+
     private static final String SQL_SELECT = "SELECT idPersona, nombre, apellido, email, telefono FROM persona;";
     private static final String SQL_INSERT = "INSERT INTO persona (nombre, apellido, email, telefono) VALUES(?,?,?,?)";
     private static final String SQL_UPDATE = "UPDATE persona SET nombre = ?, apellido = ?, email = ?, telefono = ? WHERE idPersona = ?";
     private static final String SQL_DELETE = "DELETE FROM persona WHERE idPersona = ?";
-    public List<Persona> seleccionar (){
+
+    public PersonaDAO(){
+
+    }
+
+    public PersonaDAO(Connection conexionTransaccional){
+        this.conexionTransaccional = conexionTransaccional;
+    }
+
+    public List<Persona> seleccionar () throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
         ResultSet rs = null;
@@ -21,7 +33,7 @@ public class PersonaDAO {
         List<Persona> personas = new ArrayList<>();
 
         try {
-            conn = Conexion.getConnection();
+            conn = conexionTransaccional == null ? Conexion.getConnection() : conexionTransaccional;
             stmt = conn.prepareStatement(SQL_SELECT);
             rs = stmt.executeQuery();
             while (rs.next()){
@@ -33,13 +45,13 @@ public class PersonaDAO {
                 persona = new Persona(idPersona,nombre,apellido,email,telefono);
                 personas.add(persona);
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
         }finally {
             try {
                 Conexion.close(rs);
                 Conexion.close(stmt);
-                Conexion.close(conn);
+                if (this.conexionTransaccional == null){
+                    Conexion.close(conn);
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -48,25 +60,25 @@ public class PersonaDAO {
         return personas;
     }
 
-    public int insertar(Persona persona){
+    public int insertar(Persona persona) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
         int registros = 0;
 
         try{
-            conn = Conexion.getConnection();
+            conn = conexionTransaccional == null ? Conexion.getConnection() : conexionTransaccional;
             stmt = conn.prepareStatement(SQL_INSERT);
             stmt.setString(1, persona.getNombre());
             stmt.setString(2, persona.getApellido());
             stmt.setString(3, persona.getEmail());
             stmt.setString(4, persona.getTelefono());
             registros = stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
         }finally {
             try {
                 Conexion.close(stmt);
-                Conexion.close(conn);
+                if (this.conexionTransaccional == null){
+                    Conexion.close(conn);
+                }
             } catch (SQLException e) {
                 e.printStackTrace(System.out);
             }
@@ -74,14 +86,14 @@ public class PersonaDAO {
         return registros;
     }
 
-    public int actualizar(Persona persona){
+    public int actualizar(Persona persona) throws SQLException {
         int registros = 0;
 
         Connection conn = null;
         PreparedStatement stmt = null;
 
         try {
-            conn = Conexion.getConnection();
+            conn = conexionTransaccional == null ? Conexion.getConnection() : conexionTransaccional;
             stmt = conn.prepareStatement(SQL_UPDATE);
             stmt.setString(1, persona.getNombre());
             stmt.setString(2, persona.getApellido());
@@ -89,35 +101,35 @@ public class PersonaDAO {
             stmt.setString(4, persona.getTelefono());
             stmt.setInt(5, persona.getIdPersona());
             registros = stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
         }finally {
             try {
                 Conexion.close(stmt);
-                Conexion.close(conn);
+                if (this.conexionTransaccional == null){
+                    Conexion.close(conn);
+                }
             } catch (SQLException e) {
                 e.printStackTrace(System.out);
             }
         }
         return registros;
     }
-    public int delete(int idPersona){
+    public int delete(int idPersona) throws SQLException {
         int registros = 0;
 
         Connection conn = null;
         PreparedStatement stmt = null;
 
         try {
-            conn = Conexion.getConnection();
+            conn = conexionTransaccional == null ? Conexion.getConnection() : conexionTransaccional;
             stmt = conn.prepareStatement(SQL_DELETE);
             stmt.setInt(1, idPersona);
             registros = stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace(System.out);
         }finally {
             try {
                 Conexion.close(stmt);
-                Conexion.close(conn);
+                if (this.conexionTransaccional == null){
+                    Conexion.close(conn);
+                }
             } catch (SQLException e) {
                 e.printStackTrace(System.out);
             }
--- JDBC2/src/test/java/TestManejoPersonas.java
+++ JDBC2/src/test/java/TestManejoPersonas.java
@@ -1,23 +1,49 @@
+import datos.Conexion;
 import datos.PersonaDAO;
 import domain.Persona;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TestManejoPersonas {
     public static void main(String[] args) {
-        PersonaDAO personaDao = new PersonaDAO();
 
-        //Insertando objeto de tipo persona
-        //Persona personaNueva = new Persona("Carlos", "Esparza", "cesparza@mail.com", "55654987897");
-        //personaDao.insertar(personaNueva);
-        /*Persona personaLuis = new Persona(5, "Luis", "Salas", "luispiyu@mail.com", "564987465");
-        personaDao.actualizar(personaLuis);*/
+        Connection conexion = null;
 
-        personaDao.delete(5);
-        List<Persona> personas = personaDao.seleccionar();
+        try {
+            conexion = Conexion.getConnection();
+            if (conexion.getAutoCommit()) {
+                conexion.setAutoCommit(false);
+            }
+            PersonaDAO personaDAO = new PersonaDAO(conexion);
 
-        personas.stream().map(persona -> "Persona = " + persona).forEach(System.out::println);
+            Persona persona = new Persona();
+            persona.setIdPersona(1);
+            persona.setNombre("Naomi");
+            persona.setApellido("Torres");
+            persona.setEmail("naomiTorres@mail.com");
+            persona.setTelefono("51972343218786");
+            personaDAO.actualizar(persona);
 
 
+            Persona personaNueva = new Persona();
+            personaNueva.setNombre("Carlos");
+//            personaNueva.setApellido("Ramirez99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999");
+            personaNueva.setApellido("Ramirez");
+            personaNueva.setEmail("carlitosRamirez@mail.com");
+            personaNueva.setTelefono("7465456465");
+            personaDAO.insertar(personaNueva);
+
+            conexion.commit();
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            try {
+                System.out.println("xdxdxd rollback");
+                conexion.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
     }
 }
